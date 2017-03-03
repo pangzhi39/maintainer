158e15171a602a1bee0174aeef1908201a59016b
wzh <69617457@qq.com>
2017-03-03 09:43:33 +0800

    monitor server

--- go_work/src/monitor_common/message.go
+++ go_work/src/monitor_common/message.go
@@ -19,3 +19,23 @@ type ResponseMessage struct {
    ErrorCode string
    ErrorMsg  string
 }
+
+/* mysql状态表
+CREATE TABLE mysql_status_detail(
+   ID int unsigned not null  auto_increment,
+   ClientIp varchar(32) not null,
+   Master_Ip varchar(32),
+   Master_Port varchar(32),
+   Master_DB varchar(32),
+   Master_LogFile varchar(64),
+   Master_LogPos int(16) unsigned,
+   Slave_Ip varchar(32),
+   Slave_Port varchar(32),
+   Slave_DB varchar(32),
+   Slave_LogFile varchar(64),
+   Slave_LogPos int(16) unsigned,
+   SyncErrorMsg varchar(1024),
+   primary key(ID)
+);
+
+*/
--- go_work/src/monitor_server/main.go
+++ go_work/src/monitor_server/main.go
@@ -1,26 +1,80 @@
 package main
 import (
    "fmt"
+   "log"
    "net/http"  
    "net/rpc"
    mc "monitor_common"
+   "database/sql"
+   _ "github.com/go-sql-driver/mysql"
 )
 
+const (
+   dbUser = "root"
+   dbPassword = "lscprm02"
+   sock_file = "/var/run/mysqld/mysqld.sock"
+)
+func init() {
+   connect_str := fmt.Sprintf("%s:%s@unix(%s)/monitor", dbUser, dbPassword, sock_file)
+   db, _ = sql.Open("mysql", connect_str)
+   
+}
+
+var db = &sql.DB{}
+
+// 对外提供服务
 type Monitor int
 
 func (t *Monitor) RecevieMysqlStatus (req *mc.RequestMessage_mysql, reply *mc.ResponseMessage) error {
-   fmt.Println(req)
+   fmt.Println(*req)
+   Insert(req)
 
+   reply.ErrorCode = "0"
+   reply.ErrorMsg = "交易成功"
    return nil
 }
 
-func main() {
+func Insert(req *mc.RequestMessage_mysql) {
+   str_sql := ` INSERT INTO mysql_status_detail(
+      ClientIp,
+      Master_Ip, Master_Port, Master_DB, Master_LogFile, Master_LogPos,
+      Slave_Ip, Slave_Port, Slave_DB, Slave_LogFile, Slave_LogPos,
+      SyncErrorMsg, TS)
+      VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, now())`
+
+   stmt, err := db.Prepare(str_sql)
+   if err != nil {
+       log.Fatal(err)
+   }
+   defer stmt.Close()
+
+   _, err = stmt.Exec(req.ClientIp, req.Master.Ip, req.Master.Port, req.Master.DB, req.Master.LogFile, req.Master.LogPos,
+      req.Slave.Ip, req.Slave.Port, req.Slave.DB, req.Slave.LogFile, req.Slave.LogPos, req.SyncErrorMsg)
+   if err != nil {
+       log.Fatal(err)
+   }
+   // lastId, err := res.LastInsertId()
+   // if err != nil {
+   //     log.Fatal(err)
+   // }
+   // rowCnt, err := res.RowsAffected()
+   // if err != nil {
+   //     log.Fatal(err)
+   // }
+   // log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
+} 
+
+func Listen() {
    monitor := new(Monitor)  
    rpc.Register(monitor)  
    rpc.HandleHTTP()  
 
-   err := http.ListenAndServe("127.0.0.1:3234", nil)  
+   err := http.ListenAndServe("0.0.0.0:9000", nil)  
    if err != nil {  
       fmt.Println(err.Error())  
-   }  
+   }
+}
+
+func main() {
+   Listen()
 }
